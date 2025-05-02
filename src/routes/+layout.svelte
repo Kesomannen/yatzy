1b86cc613ea525a39bcbd0df5be86d4ee3738959
Kesomannen <bobbo.nidor@gmail.com>
2025-05-02 08:37:59 +0200

    fix font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,12 +10,12 @@
 </svelte:head>
 
 <main
-	class="mx-auto flex w-full flex-col items-center justify-center gap-12 overflow-y-auto p-6 lg:flex-row"
+	class="font-fredoka mx-auto flex w-full flex-col items-center justify-center gap-12 overflow-y-auto p-6 lg:flex-row"
 >
 	{@render children()}
 </main>
 
-<div class="absolute bottom-2 left-3 flex items-center gap-1 text-gray-600 underline">
+<div class="fixed bottom-2 left-3 flex items-center gap-1 text-gray-600 underline">
 	<Icon icon="mdi:github" class="text-lg" />
 	<a href="https://github.com/Kesomannen/yatzy">Github</a>
 </div>
